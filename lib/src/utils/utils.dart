171e64eb46e43d8beb23620464efb6479e4772b7
john-scaria <johnpothanickal004@gmail.com>
2022-06-24 23:18:00 +0530

    yes/no dialog

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,4 +1,6 @@
 import 'package:attendance_app/src/models/user_type.dart';
+import 'package:attendance_app/src/ui/widgets/app_button.dart';
+import 'package:attendance_app/src/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -126,5 +128,64 @@ class Utils {
       return '$uid<##>$type';
     }
     return 'abc';
+  }
+
+  static Future<bool> yesNoDialog(
+    context, {
+    String? title,
+    String? message,
+  }) async {
+    return await showDialog<bool>(
+          context: context,
+          barrierDismissible: false, // user must tap button!
+          builder: (ctx) {
+            return AlertDialog(
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20.0),
+                ),
+              ),
+              scrollable: true,
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    title ?? '',
+                    style: const TextStyle(fontWeight: FontWeight.w900),
+                  ),
+                  Constants.verticalSpacer15,
+                  Text(message ?? ''),
+                  Constants.verticalSpacer15,
+                  Row(
+                    children: [
+                      Expanded(
+                        child: AppButton(
+                          buttonColor: Colors.white,
+                          borderSideColor: Colors.black,
+                          textColor: Colors.black,
+                          buttonText: 'NO',
+                          onTap: () => Navigator.pop(context, false),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: AppButton(
+                          buttonText: 'YES',
+                          onTap: () => Navigator.pop(
+                            context,
+                            true,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ) ??
+        false;
   }
 }
